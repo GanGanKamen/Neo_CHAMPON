565b4c1fbe148848835b3936bbab79202df999d0
GanGanKamen <m01171408c@edu.teu.ac.jp>
2019-12-02 17:57:06 +0900

    例の反射針デモを作成。ガス機器壊れたので自炊出来なくなった

--- Gururin/Assets/Scripts/Player/gururin_dead.cs
+++ Gururin/Assets/Scripts/Player/gururin_dead.cs
@@ -21,6 +21,7 @@ public class gururin_dead : MonoBehaviour
     public bool _SEPlay;
     private Text _lifeCount;
     [SerializeField] private GanGanKamen.DeadZone deadZone;
+    private PlayerMove player;
     // Start is called before the first frame update
     void Start()
     {
@@ -35,6 +36,7 @@ public class gururin_dead : MonoBehaviour
 
         _cameraBlend = _cinemachineBrain.m_DefaultBlend.m_Time;
         _SEPlay = false;
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMove>();
     }
 
     // Update is called once per frame
@@ -43,7 +45,7 @@ public class gururin_dead : MonoBehaviour
         _lifeCount.text = " " + RemainingLife.life;
 
         //コライダーを非表示にする(即死ゾーンの重複を避けるため)
-        if (_flagManager.deadZoneCol)
+        if (_flagManager.deadZoneCol && gameObject.GetComponent<BoxCollider2D>())
         {
             gameObject.GetComponent<BoxCollider2D>().enabled = false;
         }
@@ -94,6 +96,50 @@ public class gururin_dead : MonoBehaviour
             Invoke("GameOver", 2.0f);
         }
     }    
+
+    public void Dead()
+    {
+        _flagManager.deadZoneCol = true;
+        //カメラのブレンド速度を変化させる
+        _cinemachineBrain.m_DefaultBlend.m_Time = 0.7f;
+        //カメラをぐるりんに近づける
+        vCam.SetActive(true);
+        NeoConfig.isSoundFade = true;
+        CriAtom.SetCategoryVolume("BGM", 0.0f);
+        var _rb2d = player.GetComponent<Rigidbody2D>();
+        //ぐるりんの動き(操作系・重力系)を停止
+        _rb2d.constraints = RigidbodyConstraints2D.FreezeAll;
+        //顔を驚き顔に変更
+        _flagManager.surprise_Face = true;
+        // 残機を減らす
+        RemainingLife.life -= 1;
+
+        if (RemainingLife.life != 0)
+        {
+            if (_SEPlay == false)
+            {
+                _flagManager.returnGravity = true;
+                _gameOverSE.Play();
+                _SEPlay = true;
+            }
+        }
+        else if (RemainingLife.life == 0)
+        {
+            _flagManager.returnGravity = true;
+            //残機が0ならGameOverを表示
+            _gameOver.alpha = 1.0f;
+
+            //ゲームオーバー時のBGMを鳴らす
+            if (_SEPlay == false)
+            {
+                _gameOverSE.Play();
+                _SEPlay = true;
+            }
+        }
+
+        if (deadZone != null) deadZone.BossSceneReload();
+        Invoke("GameOver", 2.0f);
+    }
 
     void GameOver()
     {
