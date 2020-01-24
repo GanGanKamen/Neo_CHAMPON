ab8f1737bdaf8c8c47cef58189d89c2ecdcd7722
GanGanKamen <m01171408c@edu.teu.ac.jp>
2020-01-24 22:07:46 +0900

    ぐるりんの移動処理を仮実装

---
+++ Gururin_3D/Assets/GanGanKamen/Scripts/GururinBase.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GururinBase : MonoBehaviour
+{
+    public float speed { get; set; } 
+    public GameObject gear { get; set; }
+    public float jumpPower { get; set; }
+    private int preDirection;
+
+    public void GururinMove(float angle,bool direction)
+    {
+        var rigidbody = GetComponent<Rigidbody>();
+        var rotSpeed = angle * speed * Time.deltaTime; 
+        if (direction)
+        {
+            rigidbody.AddForce(rotSpeed, 0, 0,ForceMode.Acceleration);
+            preDirection = 1;
+        }
+        else
+        {
+            rigidbody.AddForce(-rotSpeed, 0, 0, ForceMode.Acceleration);
+            preDirection = -1;
+        }
+    }
+
+    public void SpeedReset()
+    {
+        var rigidbody = GetComponent<Rigidbody>();
+        var velocity = rigidbody.velocity.x;
+        Debug.Log(preDirection);
+        switch (preDirection)
+        {
+            case 1:
+                if (velocity > 0)
+                {
+                    velocity -= 5f * Time.deltaTime;
+
+                }
+                else
+                {
+                    velocity = 0;
+                }
+                break;
+            case -1:
+                if (velocity < 0)
+                {
+                    velocity += 5f * Time.deltaTime;
+
+                }
+                else
+                {
+                    velocity = 0;
+                }
+                break;
+        }
+        
+        rigidbody.velocity = new Vector3(velocity,
+   rigidbody.velocity.y, rigidbody.velocity.z);
+    }
+
+    public void Jump()
+    {
+        var rigidbody = GetComponent<Rigidbody>();
+        var force = new Vector3(0, jumpPower, 0);
+        rigidbody.AddForce(force, ForceMode.VelocityChange);
+    }
+}
---
+++ Gururin_3D/Assets/GanGanKamen/Scripts/PlayerCtrl.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : GururinBase
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private GameObject _gear;
+    [SerializeField] private float _jumpPower;
+
+    [SerializeField] private Canvas controllerCanvas;
+    [SerializeField] private GameObject gameController;
+    private bool isPress = false;
+    private Vector3 vecA, vecB;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        speed = _speed;
+        gear = _gear;
+        jumpPower = _jumpPower;
+    }
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TestCtrl();
+    }
+
+    /*
+    private void Operate()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 localpoint;
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(controllerCanvas.GetComponent<RectTransform>(),
+               Input.mousePosition, controllerCanvas.worldCamera, out localpoint);
+            Debug.Log(localpoint);
+            Vector2 resultPoint;
+            resultPoint = new Vector2(localpoint.x, localpoint.y - 150);
+
+            gameController.GetComponent<RectTransform>().anchoredPosition = resultPoint;
+            gameController.SetActive(true);
+            isPress = true;
+
+        }
+
+        else if(Input.GetMouseButton(0) && isPress)
+        {
+            var touchPos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+
+        }
+    }
+    */
+    private void TestCtrl()
+    {
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            if (Input.GetAxis("Horizontal") > 0)
+            {
+                base.GururinMove(Input.GetAxis("Horizontal") * 360, true);
+            }
+            else if (Input.GetAxis("Horizontal") < 0)
+            {
+                base.GururinMove(-Input.GetAxis("Horizontal") * 360, false);
+            }
+        }
+        else
+        {
+            base.SpeedReset();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            base.Jump();
+        }
+    }
+
+
+}
