b46e7ac26230375ccf98d9bf5a18c008fe1e452c
Tw3 <m0117183a5@edu.teu.ac.jp>
2020-02-15 19:08:53 +0900

    新規シーンにてアイテム2種の作成

---
+++ Gururin_3D/Assets/Tw3/Script/ItemScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemScript : MonoBehaviour
+{
+    /// <summary>
+    /// itemNumber アイテムの番号
+    /// 0…アブラ
+    /// 1…ゴールドハカセコイン
+    /// </summary>
+    
+    [SerializeField] private int itemNumber;
+
+    private PlayerStatus playerStatus;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerStatus = GameObject.Find("PlayerStatus").GetComponent<PlayerStatus>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            switch(itemNumber)
+            {
+                case 0:
+                    playerStatus.oil += 1;
+                    Destroy();
+                    break;
+
+                case 1:
+                    playerStatus.coin = true;
+                    Destroy();
+                    break;
+
+            }
+        }
+    }
+
+    private void Destroy()
+    {
+        Destroy(this.gameObject);
+    }
+}
---
+++ Gururin_3D/Assets/Tw3/Script/PlayerStatus.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStatus : MonoBehaviour
+{
+    public int oil;
+    public bool coin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        oil = 0;
+        coin = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
