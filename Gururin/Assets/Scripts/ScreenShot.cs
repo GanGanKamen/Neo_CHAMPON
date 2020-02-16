56076707803ae00c6751175185243667d777f897
GanGanKamen <m01171408c@edu.teu.ac.jp>
2020-02-16 13:06:45 +0900

    ScreenShot

---
+++ Gururin/Assets/Scripts/ScreenShot.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenShot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            ScreenCapture.CaptureScreenshot("image" + Time.time + ".png");
+        }
+        
+    }
+}
