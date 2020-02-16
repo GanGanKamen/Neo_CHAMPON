ff981540eb6d0d0c45dc017f14ec4a7a536c1e5a
50-storm <m0117019bc@edu.teu.ac.jp>
2020-02-16 12:16:35 +0900

    ピストンギミックのプロトタイプを作成

---
+++ Gururin_3D/Assets/Igarashi_Test/TestScripts/PistonRange.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ピストンの押し出し判定
+/// </summary>
+public class PistonRange : MonoBehaviour
+{
+    [SerializeField] private Piston _piston;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<PlayerCtrl>())
+        {
+            _piston.RangeHit(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.GetComponent<PlayerCtrl>())
+        {
+            _piston.RangeHit(false);
+        }
+    }
+}
---
+++ Gururin_3D/Assets/Igarashi_Test/TestScripts/TextRespawn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextRespawn : MonoBehaviour
+{
+    [SerializeField] private GameObject _respawnPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<PlayerCtrl>())
+        {
+            other.transform.position = _respawnPoint.transform.position;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
