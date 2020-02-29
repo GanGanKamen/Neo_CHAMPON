1186293ea0dadd2af4f1ebf109feee5cb9c97496
50-storm <m0117019bc@edu.teu.ac.jp>
2020-02-29 10:30:25 +0900

    リスポーン周りのスクリプトを作成

---
+++ Gururin_3D/Assets/Igarashi_Test/TestScripts/IRespawn.cs
@@ -0,0 +1,8 @@
+﻿/// <summary>
+/// interfaceを使ってみたものの使い方がよく分からんので一旦保留
+/// </summary>
+
+public interface IRespawn
+{
+    void RespawnSetting();
+}
---
+++ Gururin_3D/Assets/Igarashi_Test/TestScripts/RespawnPoint.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// リスポーン地点とぐるりんの接触判定
+/// </summary>
+
+namespace Igarashi
+{
+    public class RespawnPoint : MonoBehaviour
+    {
+        private Respawn _respawn;
+        [SerializeField] [Multiline(2)] private string memo = "リスポーン地点に設定したい場所に\nこのオブジェクトを配置してください";
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            _respawn = GameObject.Find("RespawnSet").GetComponent<Respawn>();
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.GetComponent<GanGanKamen.PlayerCtrl>())
+            {
+                // ぐるりんと接触したらリスポーン地点に設定
+                _respawn.RespawnPointSet(gameObject);
+            }
+        }
+    }
+}
