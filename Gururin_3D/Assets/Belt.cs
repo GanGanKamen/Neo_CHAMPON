01dd5f34b0ce0a29665093e60cdce256d9875d14
Tw3 <m0117183a5@edu.teu.ac.jp>
2020-03-03 19:58:07 +0900

    ベルトコンベア作成

---
+++ Gururin_3D/Assets/Belt.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Belt : MonoBehaviour
+{
+    private Beltconveyor beltConveyor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        beltConveyor = GameObject.Find("BeltconCollider").GetComponent<Beltconveyor>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(beltConveyor.right)
+        {
+            this.gameObject.transform.Translate(0.01f * beltConveyor.speed, 0, 0);
+        }
+        else
+        {
+            this.gameObject.transform.Translate(-0.01f * beltConveyor.speed, 0, 0);
+        }
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Limit"))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    
+}
---
+++ Gururin_3D/Assets/Beltconveyor.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Beltconveyor : MonoBehaviour
+{
+    [SerializeField] private GanGanKamen.GururinBase gururinBase;
+    [SerializeField] private GameObject belt, limit, player;
+    [SerializeField] private int length;
+    [SerializeField] public float speed;
+    [SerializeField] public bool right;
+    private Vector3 defaultpos;
+    private int count, generate;
+
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        belt.SetActive(false);
+        if(right)
+        {
+            limit.transform.Translate(length + 2, 0, 0);
+            for (int i = 0; i < length + 1; i++)
+            {
+                GameObject belts = Instantiate(belt) as GameObject;
+                belts.SetActive(true);
+                belts.transform.Translate(length - 1 - i, 0, 0);
+            }
+        }else
+        {
+            limit.transform.Translate(-(length + 2), 0, 0);
+            for (int i = 0; i < length + 1; i++)
+            {
+                GameObject belts = Instantiate(belt) as GameObject;
+                belts.SetActive(true);
+                belts.transform.Translate(-(length - 1 - i), 0, 0);
+            }
+        }
+        
+        
+        count = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(count * speed > 90)
+        {
+            //generate++;
+            GameObject belts = Instantiate(belt) as GameObject;
+            belts.SetActive(true);
+            if(right)
+            {
+                belts.transform.Translate(-1, 0, 0);
+            }else
+            {
+                belts.transform.Translate(1, 0, 0);
+            }
+            count = 0;
+        }
+        else
+        {
+            count++;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        var playerRb = player.GetComponent<Rigidbody>();
+        if (other.CompareTag("Player"))
+        {
+            //gururinBase.beltspeed = speed;
+            if(Input.GetMouseButton(0) == false && playerRb.velocity.x < speed/2 && playerRb.velocity.x > -speed/2)
+            {
+                gururinBase.MoveStop();
+            }
+            player.transform.Rotate(0, 0, speed * 0.9f);
+            playerRb.AddForce(1f * speed, 0, 0);
+            Debug.Log("x");
+        }
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("y");
+        }
+        
+    }
+}
