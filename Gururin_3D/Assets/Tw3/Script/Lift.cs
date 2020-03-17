2d3c911596087a22165c9ba930424afdffd88d1f
Tw3 <m0117183a5@edu.teu.ac.jp>
2020-03-17 13:54:45 +0900

    リフトの移動を距離で行えるように変更

--- Gururin_3D/Assets/Tw3/Script/Lift.cs
+++ Gururin_3D/Assets/Tw3/Script/Lift.cs
@@ -7,16 +7,24 @@ public class Lift : MonoBehaviour
     [SerializeField] private GanGanKamen.GururinBase gururinBase;
     [SerializeField] private GameObject player;
     //move 動き 
-    [SerializeField] private int move, moveMode, count, countMax;
-    [SerializeField] private float speed;
+    [SerializeField] private int move, moveMode, interval;
+    [SerializeField] private float speed, distance;
 
-
+    private int count, countMax, intervalcount;
+    private bool firsttime, isInterval;
+    private Vector3 firstpos;
 
 
     // Start is called before the first frame update
     void Start()
     {
         count = 0;
+        firsttime = true;
+        firstpos = this.gameObject.transform.position;
+        if(interval > 0)
+        {
+            isInterval = true;
+        }
     }
 
     // Update is called once per frame
@@ -29,45 +37,158 @@ public class Lift : MonoBehaviour
     {
         switch (move)
         {
-            case 0://上下移動
+            case 0://上下移動 上→下
                 moveMode = 0;
-                if (count >= 0 && count < countMax/4)
+                //初回動作
+                if (this.gameObject.transform.position.y - firstpos.y < distance && firsttime)
                 {
                     this.gameObject.transform.Translate(0, speed, 0);
                 }
-                else if(count >= countMax/4 && count < countMax * 3/4)
+                else if(this.gameObject.transform.position.y - firstpos.y >= distance && firsttime)
                 {
+                    countMax = count * 2;
+                    this.gameObject.transform.Translate(0, -speed, 0);
+                    firsttime = false;
+                }
+                //往復動作以降
+                else if(count >= 0 && count < countMax / 2 && !firsttime)
+                {
+                    this.gameObject.transform.Translate(0, speed, 0);
+                    if (interval > 0 && !isInterval)
+                    {
+                        isInterval = true;
+                    }
+                }
+                else if(count == countMax /2 && !firsttime)
+                {
+                    /*if(isInterval)//インターバルでの停止動作1
+                    {
+                        if(intervalcount < interval)
+                        {
+                            intervalcount++;
+                            count--;
+                        }
+                        else if (intervalcount == interval)
+                        {
+                            intervalcount = 0;
+                            isInterval = false;
+                        }
+                    }
+                    else
+                    {
+                        this.gameObject.transform.Translate(0, -speed, 0);
+                    }*/
                     this.gameObject.transform.Translate(0, -speed, 0);
                 }
-                else if(count >= countMax * 3/4 && count < countMax)
+                else if(count > countMax / 2 && count < countMax && !firsttime)
                 {
-                    this.gameObject.transform.Translate(0, speed, 0);
+                    this.gameObject.transform.Translate(0, -speed, 0);
+                    if(interval > 0 && !isInterval)
+                    {
+                        isInterval = true;
+                    }
                 }
-                else if(count == countMax)
+                else if(count == countMax && !firsttime)
                 {
+                    /*if(isInterval)//インターバルでの停止動作2
+                    {
+                        if (intervalcount < interval)
+                        {
+                            intervalcount++;
+                            count--;
+                        }
+                        else if (intervalcount == interval)
+                        {
+                            intervalcount = 0;
+                            isInterval = false;
+                        }
+                    }
+                    else
+                    {
+                        this.gameObject.transform.Translate(0, speed, 0);
+                        count = 0;
+                    }*/
                     this.gameObject.transform.Translate(0, speed, 0);
                     count = 0;
                 }
                 break;
 
             case 1://左右移動
-                if (count >= 0 && count < countMax/4)
+                //初回動作
+                if (this.gameObject.transform.position.x - firstpos.x < distance && firsttime)
                 {
                     moveMode = 1;
                     this.gameObject.transform.Translate(speed, 0, 0);
                 }
-                else if (count >= countMax/4 && count < countMax * 3/4)
+                else if (this.gameObject.transform.position.x - firstpos.x >= distance && firsttime)
                 {
+                    countMax = count * 2;
+                    moveMode = 2;
+                    this.gameObject.transform.Translate(-speed, 0, 0);
+                    firsttime = false;
+                }
+
+                //往復動作以降
+                else if (count >= 0 && count < countMax / 2 && !firsttime)
+                {
+                    moveMode = 1;
+                    this.gameObject.transform.Translate(speed, 0, 0);
+                    if (interval > 0 && !isInterval)
+                    {
+                        isInterval = true;
+                    }
+                }
+                else if (count == countMax / 2 && !firsttime)
+                {
+                    /*if(isInterval)//インターバルでの停止動作1
+                    {
+                        if(intervalcount < interval)
+                        {
+                            intervalcount++;
+                            count--;
+                        }
+                        else if (intervalcount == interval)
+                        {
+                            intervalcount = 0;
+                            isInterval = false;
+                        }
+                    }
+                    else
+                    {
+                        this.gameObject.transform.Translate(0, -speed, 0);
+                    }*/
                     moveMode = 2;
                     this.gameObject.transform.Translate(-speed, 0, 0);
                 }
-                else if (count >= countMax * 3/4 && count < countMax)
+                else if (count > countMax / 2 && count < countMax && !firsttime)
                 {
-                    moveMode = 1;
-                    this.gameObject.transform.Translate(speed, 0, 0);
+                    moveMode = 2;
+                    this.gameObject.transform.Translate(-speed, 0, 0);
+                    if (interval > 0 && !isInterval)
+                    {
+                        isInterval = true;
+                    }
                 }
-                else if (count == countMax)
+                else if (count == countMax && !firsttime)
                 {
+                    /*if(isInterval)//インターバルでの停止動作2
+                    {
+                        if (intervalcount < interval)
+                        {
+                            intervalcount++;
+                            count--;
+                        }
+                        else if (intervalcount == interval)
+                        {
+                            intervalcount = 0;
+                            isInterval = false;
+                        }
+                    }
+                    else
+                    {
+                        this.gameObject.transform.Translate(0, speed, 0);
+                        count = 0;
+                    }*/
                     moveMode = 1;
                     this.gameObject.transform.Translate(speed, 0, 0);
                     count = 0;
