dc335d1830c9bd082ca56ccbc923987b94f6db55
50-storm <m0117019bc@edu.teu.ac.jp>
2020-03-23 13:17:40 +0900

    空中歯車の不具合を修正、歯車ピストンの押し出し判定を調整

--- Gururin_3D/Assets/Igarashi/Scripts/Respawn/ChangeDeadZoneSpeed.cs
+++ Gururin_3D/Assets/Igarashi/Scripts/Respawn/ChangeDeadZoneSpeed.cs
@@ -7,26 +7,31 @@ using UnityEngine;
 /// 実質迫りくる炎専用
 /// </summary>
 
-public class ChangeDeadZoneSpeed : MonoBehaviour
+namespace Igarashi
 {
-    [SerializeField] [Header("即死ゾーンの移動速度を変更")] [Range(0.1f, 2.0f)] private float changeSpeed;
-    [SerializeField] [Multiline(2)] private string memo = "即死ゾーンの速度を変更したい場所に\n" +
-                                                                                     "このオブジェクトを配置してください";
-
-    // Start is called before the first frame update
-    void Start()
+    public class ChangeDeadZoneSpeed : MonoBehaviour
     {
-        // Rigidbodyを付与
-        var _rigidbody = GetComponent<Rigidbody>() == null ? gameObject.AddComponent<Rigidbody>() : GetComponent<Rigidbody>();
-        _rigidbody.isKinematic = true;
-    }
+        [SerializeField] [Header("即死ゾーンの移動速度を変更")] [Range(0.1f, 2.0f)] private float changeSpeed;
+        [SerializeField]
+        [Multiline(2)]
+        private string memo = "即死ゾーンの速度を変更したい場所に\n" +
+                                            "このオブジェクトを配置してください";
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.GetComponent<MoveObject>())
+        // Start is called before the first frame update
+        void Start()
         {
-            var moveDeadZone = other.GetComponent<MoveObject>();
-            moveDeadZone.SpeedSetting(changeSpeed);
+            // Rigidbodyを付与
+            var _rigidbody = GetComponent<Rigidbody>() == null ? gameObject.AddComponent<Rigidbody>() : GetComponent<Rigidbody>();
+            _rigidbody.isKinematic = true;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.GetComponent<MoveObject>())
+            {
+                var moveDeadZone = other.GetComponent<MoveObject>();
+                moveDeadZone.SpeedSetting(changeSpeed);
+            }
         }
     }
 }
