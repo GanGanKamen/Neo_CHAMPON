62454dd66768b8a60722f9e296a45a3ec2a06b78
GanGanKamen <m01171408c@edu.teu.ac.jp>
2020-03-24 17:26:48 +0900

    銀扉の更新

--- Gururin_3D/Assets/GanGanKamen/Scripts/Gimmick/TeleportPoint.cs
+++ Gururin_3D/Assets/GanGanKamen/Scripts/Gimmick/TeleportPoint.cs
@@ -4,14 +4,15 @@ using UnityEngine;
 
 namespace GanGanKamen
 {
-    public class TranportPoint : MonoBehaviour
+    public class TeleportPoint : MonoBehaviour
     {
-        [Header("パート")] [SerializeField] [Range(1, 2)] int partNum;
-        [Header("ステージパート")] [SerializeField] GameObject[] stageParts;
+        [Header("現在パート")] [SerializeField] GameObject nowPart;
         [Header("移動先")] [SerializeField] Transform destination;
+        [Header("移動先パート")] [SerializeField] GameObject nextPart;
         [Header("フェードイン時間")] [SerializeField] float fadeinTime;
         [Header("待ち時間")] [SerializeField] float waitTime;
         [Header("フェードアウト時間")] [SerializeField] float fadeoutTime;
+        [Header("隠扉")] [SerializeField] bool extra;
         // Start is called before the first frame update
         void Start()
         {
@@ -38,12 +39,13 @@ namespace GanGanKamen
             player.MoveStop();
             player.ProhibitControll();
             Fader.FadeInAndOutBlack(fadeinTime, waitTime, fadeoutTime);
-            stageParts[partNum].SetActive(true);
+            nextPart.SetActive(true);
             yield return new WaitForSeconds(fadeinTime + waitTime / 2);
             var pos = new Vector3(destination.position.x, destination.position.y, 0);
             player.transform.position = pos;
             yield return new WaitForSeconds(waitTime / 2 + fadeoutTime);
-            Destroy(stageParts[partNum - 1]);
+            if (extra) nowPart.SetActive(false);
+            else Destroy(nowPart);
             player.PermitControll();
             yield break;
         }
